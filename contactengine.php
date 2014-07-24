393559feaff9777224532493e2a73840de75bd2d
billy5521 <billy5521@gmail.com>
2014-07-24 16:57:27 -0700

    add contact form action

---
+++ contactengine.php
@@ -0,0 +1,44 @@
+<?php
+
+$EmailTo = "billy5521@gmail.com";
+$Subject = "Nice & Simple Contact Form by CSS-Tricks";
+$Name = Trim(stripslashes($_POST['Name'])); 
+$Phone = Trim(stripslashes($_POST['Phone'])); 
+$Email = Trim(stripslashes($_POST['Email'])); 
+$Message = Trim(stripslashes($_POST['Message'])); 
+$EmailFrom = $Email;
+
+
+// validation
+$validationOK=true;
+if (!$validationOK) {
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.htm\">";
+  exit;
+}
+
+// prepare email body text
+$Body = "";
+$Body .= "Name: ";
+$Body .= $Name;
+$Body .= "\n";
+$Body .= "Tel: ";
+$Body .= $Phone;
+$Body .= "\n";
+$Body .= "Email: ";
+$Body .= $Email;
+$Body .= "\n";
+$Body .= "Message: ";
+$Body .= $Message;
+$Body .= "\n";
+
+// send email 
+$success = mail($EmailTo, $Subject, $Body, "From: <$EmailFrom>");
+
+// redirect to success page 
+if ($success){
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=thanks.html\">";
+}
+else{
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.htm\">";
+}
+?>

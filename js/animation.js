b635fda8d927a415461415e13d981dd2a3197ea8
billy5521 <billy5521@gmail.com>
2014-07-29 16:04:08 -0700

    fixes

---
+++ js/animation.js
@@ -0,0 +1,6 @@
+$(".img-container").mouseenter(function(){
+	  $(this).find(".hover").fadeIn("200");
+	});
+	$(".img-container").mouseleave(function(){
+	  $(this).find(".hover").fadeOut("200");
+	});
